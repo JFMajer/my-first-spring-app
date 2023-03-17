cae11526c9231df01d346d734c95bebdadc6e0c9
JFMajer <jakubmajer@live.com>
2023-03-17 20:11:26 +0100

    added second endpoint

--- src/main/java/com/spring/firstdemo/firstspringapp/rest/FunRestController.java
+++ src/main/java/com/spring/firstdemo/firstspringapp/rest/FunRestController.java
@@ -1,14 +1,22 @@
 package com.spring.firstdemo.firstspringapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
     // expose "/" endpoint and return Hello, World!
-
+    @Value("${name}")
+    private String name;
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, world!";
+        return "Hello " + name;
+    }
+
+    // expose "info" endpoint and return some info
+    @GetMapping("/info")
+    public String sayInfo() {
+        return "This is a Spring Boot app";
     }
 }
