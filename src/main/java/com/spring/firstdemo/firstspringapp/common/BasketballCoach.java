c6b1bdd65947afa9d45f30139c0c07cb184c1a06
JFMajer <jakubmajer@live.com>
2023-03-20 19:31:51 +0100

    added second coach

--- src/main/java/com/spring/firstdemo/firstspringapp/common/BasketballCoach.java
+++ src/main/java/com/spring/firstdemo/firstspringapp/common/BasketballCoach.java
@@ -1,11 +1,10 @@
 package com.spring.firstdemo.firstspringapp.common;
 
-import com.spring.firstdemo.firstspringapp.common.Coach;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
+public class BasketballCoach implements Coach {
     public String getDailyWorkout() {
-        return "Practice fast bowling for 20 minutes!";
+        return "Practice your jump shot for 20 minutes";
     }
 }
--- src/main/java/com/spring/firstdemo/firstspringapp/rest/DemoController.java
+++ src/main/java/com/spring/firstdemo/firstspringapp/rest/DemoController.java
@@ -1,7 +1,9 @@
 package com.spring.firstdemo.firstspringapp.rest;
 
+import com.spring.firstdemo.firstspringapp.common.BasketballCoach;
 import com.spring.firstdemo.firstspringapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
-
+    private Coach secondCoach;
+    // dependency injection with qualifier using setter injection
     @Autowired
-    public DemoController(Coach myCoach) {
+    public void setMyCoach(BasketballCoach myCoach) {
         this.myCoach = myCoach;
     }
+
+    // dependency injection with qualifier using constructor injection
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach secondCoach) {
+        this.secondCoach = secondCoach;
+    }
+
 
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/workout-cricket")
+    public String getDailyWorkout2() {
+        return secondCoach.getDailyWorkout();
     }
 
 }
