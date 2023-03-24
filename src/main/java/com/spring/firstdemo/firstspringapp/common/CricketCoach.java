5b39d19ca45af7534de23b24e937692adc836572
JFMajer <jakubmajer@live.com>
2023-03-24 20:26:06 +0100

    beans scope

--- src/main/java/com/spring/firstdemo/firstspringapp/common/CricketCoach.java
+++ src/main/java/com/spring/firstdemo/firstspringapp/common/CricketCoach.java
@@ -1,10 +1,19 @@
 package com.spring.firstdemo.firstspringapp.common;
 
 import com.spring.firstdemo.firstspringapp.common.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
     public String getDailyWorkout() {
         return "Practice fast bowling for 20 minutes!";
     }
--- src/main/java/com/spring/firstdemo/firstspringapp/rest/DemoController.java
+++ src/main/java/com/spring/firstdemo/firstspringapp/rest/DemoController.java
@@ -11,17 +11,24 @@ public class DemoController {
 
     private Coach myCoach;
     private Coach secondCoach;
+    private Coach anotherCoach;
 
     // dependency injection with qualifier using setter injection
     @Autowired
     public void setMyCoach(@Qualifier("basketballCoach") Coach myCoach) {
+        System.out.println("In constructor of basketballCoach: " + getClass().getSimpleName());
         this.myCoach = myCoach;
     }
 
     // dependency injection with qualifier using constructor injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach secondCoach) {
+    public DemoController(
+            @Qualifier("cricketCoach") Coach secondCoach,
+            @Qualifier("cricketCoach") Coach anotherCoach
+    ) {
+        System.out.println("In constructor: of cricketCoach: " + getClass().getSimpleName());
         this.secondCoach = secondCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/workout")
@@ -34,4 +41,20 @@ public class DemoController {
         return secondCoach.getDailyWorkout();
     }
 
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing two beans: " + (secondCoach == anotherCoach);
+    }
+
 }
+
+// Dependency injection multiple beans with qualifier - constructor injection
+//@Autowired
+//public DemoController(
+//    @Qualifier("cricketCoach") Coach secondCoach,
+//    @Qualifier("basketballCoach") Coach myCoach
+//) {
+//    this.secondCoach = secondCoach;
+//    this.myCoach = myCoach;
+//}
+//)
