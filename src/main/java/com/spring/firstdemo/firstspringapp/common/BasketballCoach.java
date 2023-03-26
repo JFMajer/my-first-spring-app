de4c263d8941a5188cff79e0e85248cae8acb978
JFMajer <jakubmajer@live.com>
2023-03-26 14:40:49 +0200

    post construct and pre destroy

--- src/main/java/com/spring/firstdemo/firstspringapp/common/BasketballCoach.java
+++ src/main/java/com/spring/firstdemo/firstspringapp/common/BasketballCoach.java
@@ -1,8 +1,21 @@
 package com.spring.firstdemo.firstspringapp.common;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BasketballCoach implements Coach {
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("BasketballCoach: inside method doMyStartupStuff");
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("BasketballCoach: inside method doMyCleanupStuff");
+    }
+
     public String getDailyWorkout() {
         return "Practice your jump shot for 20 minutes";
     }
